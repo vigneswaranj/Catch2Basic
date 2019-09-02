973a353df50cdc41a7e436a5993a562b7aad3ee4
vigneswaran <vigneswaran.jagadeesan@gmail.com>
2019-09-02 20:16:24 +0530

    Added structures for modularity

--- test_main.cpp
+++ test_main.cpp
@@ -1,15 +1,16 @@
 
-#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("First test", "[Simple]")
+#define TESTCASE_NAME(test_name) "SRCmodule - " test_name
+
+TEST_CASE(TESTCASE_NAME("TestCase1"), "[className][methodName]")
 {
 	int a;
 	a = 1;
 	REQUIRE(a == 1);
 }
 
-TEST_CASE("Second test", "[Simple]")
+TEST_CASE(TESTCASE_NAME("TestCase2"), "[className][methodName]")
 {
 	int a;
 	a = 2;
